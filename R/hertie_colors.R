09d268a119975bc37c3d6bea05e141c454d75dd6
Lisa Oswald <lisao.kassel@gmail.com>
2021-11-22 18:16:26 +0100

    add marcel's stuff

---
+++ R/hertie_colors.R
@@ -0,0 +1,47 @@
+#' @rdname HertieRed
+#' @md
+#' @title xx
+#' @description 
+#' @format 
+#' @export
+hertiered <- "firebrick4"
+
+#' @rdname HertieRedLight
+#' @md
+#' @title xxxx
+#' @description
+#' @format
+#' @export
+hertieredlight <- "indianred1"
+
+#' @rdname HertieBlue
+#' @md
+#' @title xx
+#' @description
+#' @format
+#' @export
+hertieblue <- "cadetblue3"
+
+#' @rdname HertieBlueLight
+#' @md
+#' @title x
+#' @description
+#' @format
+#' @export
+hertiebluelight <- "cadetblue1"
+
+#' @rdname HertieMud
+#' @md
+#' @title xxxxx
+#' @description
+#' @format
+#' @export
+hertiemud <- "antiquewhite3"
+
+#' @rdname HertieMudLight
+#' @md
+#' @title xxxxxxx
+#' @description
+#' @format
+#' @export
+hertiemudlight <- "antiquewhite1"
